8a00c497473d695babb78e336eeb52829a8f6657
peijian.wang <519955464@qq.com>
2019-05-26 21:52:26 +0800

    删除无用代码，格式化代码
    Signed-off-by: peijian.wang <519955464@qq.com>

--- redis-cluster/redis-start.sh
+++ redis-cluster/redis-start.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 # 环境变量
-
 # $MASTER_PASSWORD 主机密码
 
-
 # 修改配置文件基本内容
-# 主从节点配置文件
 # **********************************************************************************
 sed -i "s/protected-mode yes/protected-mode no/g" /redis.conf
 sed -i "s/daemonize yes/daemonize no/g"           /redis.conf
@@ -14,10 +11,11 @@ sed -i "s/# masterauth <master-password>/masterauth '$MASTER_PASSWORD'/g"       
 sed -i "s/# requirepass foobared/requirepass '$MASTER_PASSWORD'/g"          /redis.conf
 # **********************************************************************************
 
-echo "启动Redis服务"
-
 # redis 集群配置
+# # **********************************************************************************
 sed -i "s/# cluster-enabled yes/cluster-enabled yes/g" /redis.conf
 sed -i "s/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-6379.conf/g" /redis.conf
+# **********************************************************************************
 
+echo "启动Redis服务"
 redis-server /redis.conf

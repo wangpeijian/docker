db2603775094f6e2dddc5add86b07a9d8d1bfa4e
wangpeijian <wangpj@citic.com>
2019-05-27 14:04:51 +0800

    添加死循环，保证运行redis-cli的容器不退出

--- redis-cluster/redis-cluster-center/redis-start.sh
+++ redis-cluster/redis-cluster-center/redis-start.sh
@@ -12,6 +12,10 @@ echo "启动Redis-cli"
 (
 sleep 1
 echo "yes"
-) | redis-cli -a 123456 --cluster create 172.99.0.2:6379 
-172.99.0.3:6379 172.99.0.4:6379 172.99.0.5:6379 172.99.0.6:6379 
-172.99.0.7:6379 --cluster-replicas 1
+) | redis-cli -a 123456 --cluster create 172.99.0.2:6379 172.99.0.3:6379 172.99.0.4:6379 172.99.0.5:6379 172.99.0.6:6379 172.99.0.7:6379 --cluster-replicas 1
+
+while true
+do
+    sleep 10
+    echo "保证容器进程不关闭"
+done
